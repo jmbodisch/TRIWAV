78fcca563651220b7d862550b98e3802c5f06993
Jordan Bodisch <jmb669@drexel.edu>
2017-03-14 22:24:36 -0400

    Adding basic elements. Kinda sloppily, too, but this is prototype stuff. Boing

---
+++ TRIWAV/Assets/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public double score;
+	public int health;
+	public int combo;
+	public int bpm;
+	public int scrollSpeed;
+
+	void Start()
+	{
+		score = 0;
+		health = 100;
+	}
+}
