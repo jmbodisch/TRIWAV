1c2eea3c3d28d49c5c4e7b687edde550c6264461
Boing <boingloing@gmail.com>
2017-03-21 19:38:19 -0400

    Adding input handlers and such. Did way too much for one commit. Refactor very necessary. Boing

--- TRIWAV/Assets/Scripts/Constants.cs
+++ TRIWAV/Assets/Scripts/Constants.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Constants {
-	public const double PERFECT = 0.033;
-	public const double GREAT = 0.092;
-	public const double GOOD = 0.142;
-	public const double POOR = 0.225;
+	public const float PERFECT = 0.033f;
+	public const float GREAT = 0.092f;
+	public const float GOOD = 0.142f;
+	public const float POOR = 0.225f;
 }
--- TRIWAV/Assets/Scripts/GameController.cs
+++ TRIWAV/Assets/Scripts/GameController.cs
@@ -8,18 +8,27 @@ public class GameController : MonoBehaviour {
 	public static int health;
 	public static int combo;
 	public static int bpm;
-	public static double scrollSpeed;
+	public static float scrollSpeed;
 	public GUIText scoreText, healthText, comboText, bpmText;
+
+	public GameObject topLeft, topRight, bottom;
+	public GameObject note;
+
+	public List<GameObject> topLeftNotes;
+	private List<GameObject> topRightNotes;
+	private List<GameObject> bottomNotes;
 
 	void Start()
 	{
 		score = 0;
 		health = 100;
 		combo = 0;
-		scrollSpeed = 10;
+		scrollSpeed = .25f;
 		bpm = 90;
 
-		Instantiate (new Note (GameObject.Find("TopLeft").transform, scrollSpeed));
+		topLeftNotes = new List<GameObject> ();
+		topRightNotes = new List<GameObject> ();
+		bottomNotes = new List<GameObject> ();
 	}
 
 	void Update()
@@ -28,5 +37,68 @@ public class GameController : MonoBehaviour {
 		healthText.text = health.ToString ();
 		comboText.text = combo.ToString ();
 		bpmText.text = bpm.ToString ();
+
+		//Keyboard handlers
+		if(Input.GetKeyDown (KeyCode.A))
+			makeTopLeft ();
+		if (Input.GetKeyDown (KeyCode.S))
+			makeTopRight ();
+		if (Input.GetKeyDown (KeyCode.Z))
+			makeBottom ();
+
+		if (Input.GetKeyDown (KeyCode.G))
+			topLeftPress ();
+		if (Input.GetKeyDown (KeyCode.H))
+			topRightPress ();
+		if (Input.GetKeyDown (KeyCode.B))
+			bottomPress ();
+	}
+
+	private void makeTopLeft()
+	{
+		var newNote = Instantiate (note);
+		newNote.GetComponent<Note>().Initialize(topLeft.transform, scrollSpeed);
+		topLeftNotes.Add (newNote);
+	}
+
+	private void makeTopRight()
+	{
+		var newNote = Instantiate (note);
+		newNote.GetComponent<Note>().Initialize(topRight.transform, scrollSpeed);
+		topRightNotes.Add (newNote);
+	}
+
+	private void makeBottom()
+	{
+		var newNote = Instantiate (note);
+		newNote.GetComponent<Note>().Initialize(bottom.transform, scrollSpeed);
+		bottomNotes.Add (newNote);
+	}
+
+	private void topLeftPress()
+	{
+		if (topLeftNotes.Count > 0) {
+			GameObject note = topLeftNotes [0];
+			topLeftNotes.Remove (note);
+			note.GetComponent<Note> ().Kill ();
+		}
+	}
+
+	private void bottomPress()
+	{
+		if (bottomNotes.Count > 0) {
+			GameObject note = bottomNotes [0];
+			bottomNotes.Remove (note);
+			note.GetComponent<Note> ().Kill ();
+		}
+	}
+
+	private void topRightPress()
+	{
+		if (topRightNotes.Count > 0) {
+			GameObject note = topRightNotes [0];
+			topRightNotes.Remove (note);
+			note.GetComponent<Note> ().Kill ();
+		}
 	}
 }
