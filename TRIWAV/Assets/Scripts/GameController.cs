93bc14e77c0e8149f291c048745ac37930bf5015
Boing <boingloing@gmail.com>
2017-09-18 20:37:48 -0400

    Updated the scene to have a working health bar. Boing

--- TRIWAV/Assets/Scripts/GameController.cs
+++ TRIWAV/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
@@ -11,6 +12,7 @@ public class GameController : MonoBehaviour {
 	public static float scrollSpeed;
 	public static string judge;
 	public GUIText scoreText, healthText, comboText, bpmText, timingText;
+	public Slider healthSlider;
 
 	public GameObject topLeft, topRight, bottom;
 	public GameObject note;
@@ -32,6 +34,7 @@ public class GameController : MonoBehaviour {
 		healthText.text = health.ToString ();
 		comboText.text = combo.ToString ();
 		bpmText.text = bpm.ToString ();
+		healthSlider.value = health;
 		timingText.text = judge;
 
 		//Keyboard handlers
