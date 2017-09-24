bba36022942f35ea337054e4991289859935e4c5
Boing <boingloing@gmail.com>
2017-09-24 02:39:41 -0400

    Added some sort of button/text to every scene to make them distinguishable. Boing

--- TRIWAV/Assets/Scripts/ResultsManager.cs
+++ TRIWAV/Assets/Scripts/ResultsManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour {
-	
-	public void GoToSongSelect() {
+public class ResultsManager : MonoBehaviour {
+
+	public void songSelect() {
 		SceneManager.LoadScene ("SongSelect");
 	}
-		
+
+	public void exit() {
+		SceneManager.LoadScene ("Welcome");
+	}
 }
