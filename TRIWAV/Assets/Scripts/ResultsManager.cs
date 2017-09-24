105fa4a6cae5241fed60766507169c55814bd94d
Boing <boingloing@gmail.com>
2017-09-24 19:31:13 -0400

    Added a function to ResultsManger that updates the text on the results screen with the saved judgments and final score. Boing

--- TRIWAV/Assets/Scripts/ResultsManager.cs
+++ TRIWAV/Assets/Scripts/ResultsManager.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ResultsManager : MonoBehaviour {
+	
+	public Text resultText;
+
+	void Start() {
+		resultText.text = $"Perfects: {PlayerStats.perfects}\nGreats: {PlayerStats.greats}\nGoods: {PlayerStats.goods}\nPoors: {PlayerStats.poors}\n\nScore: {PlayerStats.score}";
+	}
 
 	public void songSelect() {
 		SceneManager.LoadScene ("SongSelect");
