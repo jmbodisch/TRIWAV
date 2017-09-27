d40926bc77486e0ee7e76dc26a07325bee0ed11f
Boing <boingloing@gmail.com>
2017-09-26 22:12:42 -0400

    Initial commit for parser. Making a struct for notes that the game can read to spawn, and a class for the more heafty Song object, that will represent everything the Gameplay screen needs to perform the song. Hopefully. Boing

---
+++ TRIWAV/Assets/Scripts/SongParser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+public struct noteSpawn {
+	float time;
+	string type;
+}
+
+public class Song {
+	public float bpm;
+	public string title;
+	public string artist;
+	public string music;
+	public float offset;
+	public List<noteSpawn> topLeftNotes;
+	public List<noteSpawn> topRightNotes;
+	public List<noteSpawn> bottomNotes;
+}
+
+public class SongParser {
+
+}
